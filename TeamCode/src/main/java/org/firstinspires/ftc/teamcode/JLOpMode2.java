56ef97ef654a8458d458838ecc3e32188f3042d1
Alan Smith <alan@randomsmiths.com>
2019-06-03 19:38:21 -0400

    Add the JLOpMode2 class
    This is a little story about that time I added an OpMode class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLOpMode2.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+@Disabled
+public class JLOpMode2 extends OpMode {
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+
+    }
+}
