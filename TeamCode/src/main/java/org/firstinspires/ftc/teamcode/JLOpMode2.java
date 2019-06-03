79b20b848651f47c6c595b4a895fc5d500a2aac6
Alan Smith <alan@randomsmiths.com>
2019-06-03 19:39:12 -0400

    Add comments to JLOpMode2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLOpMode2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLOpMode2.java
@@ -10,7 +10,7 @@ public class JLOpMode2 extends OpMode {
     // Code to run ONCE when the driver hits INIT
     @Override
     public void init() {
-
+        // do stuff here
     }
 
     // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
