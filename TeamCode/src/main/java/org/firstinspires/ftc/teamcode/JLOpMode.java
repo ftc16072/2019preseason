5987a1e15a097b87ab8ef323d45bb3ee3286eb95
Jordan Liggitt <jordan@liggitt.net>
2019-06-02 20:24:40 -0400

    Updated JLOpMode comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLOpMode.java
@@ -4,18 +4,22 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp()
 @Disabled
 public class JLOpMode extends OpMode {
+    private int iterations = 0;
+
     // Code to run ONCE when the driver hits INIT
     @Override
     public void init() {
-
     }
 
-    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    // Added some comments about what this does
     @Override
     public void loop() {
-
+        this.telemetry.addData("JL", this.iterations);
+        this.iterations = this.iterations + 1;
     }
 }
