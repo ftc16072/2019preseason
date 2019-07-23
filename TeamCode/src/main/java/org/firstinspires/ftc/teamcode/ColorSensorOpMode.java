2e8e70835b7f8004a0e587c4b5cf3a9902741736
Preeti <preetithirukonda@gmail.com>
2019-07-22 20:19:46 -0400

    Added color and distance sensor programs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class ColorSensorOpMode extends OpMode {
+    private Robot robot = new Robot();
+
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        telemetry.addData("getGreen", robot.getGreen());
+        telemetry.addData("Red", robot.getRed());
+        telemetry.addData("blue", robot.getBlue());
+    }
+}
